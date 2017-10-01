4310948497b40fc4814669a984a3f41b3f03fc51
yoinergomez <yoiner.gomez22@gmail.com>
2017-10-01 10:12:31 -0500

    Fixed jasmine import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {} from 'jasmine';
 
 import { HomePage } from '../pages/home/home';
 @Component({
