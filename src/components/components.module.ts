23cf1ac31759ad44b306c7190f145964d69f0672
yoinergomez <yoiner.gomez22@gmail.com>
2017-10-01 09:02:54 -0500

    Added new component 'Arithmetic'

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ArithmeticComponent } from './arithmetic/arithmetic';
+@NgModule({
+	declarations: [ArithmeticComponent],
+	imports: [],
+	exports: [ArithmeticComponent]
+})
+export class ComponentsModule {}
