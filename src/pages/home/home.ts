cc7ed4545caa3c61182baf1b727388b4a7734fea
yoiner.gomez <yoiner.gomez22@gmail.com>
2017-10-11 22:04:10 -0500

    Added date components in home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RequestSearchRooms } from "../../interfaces/request.interface";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  request: RequestSearchRooms;
 
+  constructor(public navCtrl: NavController) {
+    this.request = {} as RequestSearchRooms;
   }
 
 }
