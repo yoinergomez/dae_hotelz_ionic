14df53443c14526e685fa98f31e66f0dad35382f
yoinergomez <yoiner.gomez22@gmail.com>
2017-10-11 21:56:07 -0500

    Got rooms from dummy data in HotelzProvider

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,9 @@
 import { async, TestBed } from '@angular/core/testing';
 import { IonicModule, Platform } from 'ionic-angular';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { MyApp } from './app.component';
+
 import {
   PlatformMock,
   StatusBarMock,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HotelzProvider } from '../../providers/hotelz/hotelz';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  rooms: any = [];
+  constructor(public navCtrl: NavController, private _hotelzProvider: HotelzProvider) {
+    _hotelzProvider.getAvalaibleRooms().then((response)=>{
+      this.rooms = response;
+      console.log(this.rooms);
+    });
   }
+
+
 
 }
