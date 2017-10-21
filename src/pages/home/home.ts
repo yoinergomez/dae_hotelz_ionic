c3dd9af9c59e0a5e618c7e33accedd3755ffbf02
yoinergomez <yoiner.gomez22@gmail.com>
2017-10-20 23:10:25 -0500

    Add valitation to room's form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HotelzProvider } from '../../providers/hotelz/hotelz';
-import { RequestSearchRooms } from "../../interfaces/request.interface";
 import { ListRoomsPage } from "../list-rooms/list-rooms";
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  request: RequestSearchRooms;
   rooms: any = [];
+  formSearchRooms: FormGroup;
 
-  constructor(public navCtrl: NavController, private _hotelzProvider: HotelzProvider) {
-    this.request = {} as RequestSearchRooms;
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, private _hotelzProvider: HotelzProvider) {
+    this.formSearchRooms = this.formBuilder.group({
+      arrive_date: [null, Validators.required],
+      leave_date: [null, Validators.required],
+      city: [null, Validators.required],
+      hosts: [null, Validators.required],
+      room_type: [null, Validators.required]
+    });
   }
 
   getRooms(event) {
     this._hotelzProvider.getAvalaibleRooms().then((response) => {
+      console.log(this.formSearchRooms.value)
       let result: any = response
       this.navCtrl.push(ListRoomsPage, {response: result})
     })
