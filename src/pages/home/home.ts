3904d5f8f3f738efb1a5c8d91e0077f9ed2bee10
Maria Camila Gómez <camigomez@experimentality.co>
2017-10-22 11:29:26 -0500

    Show alls hotels rooms

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { HotelzProvider } from '../../providers/hotelz/hotelz';
 import { RequestSearchRooms } from "../../interfaces/request.interface";
 import { ListRoomsPage } from "../list-rooms/list-rooms";
 
@@ -12,23 +11,13 @@ export class HomePage {
   request: RequestSearchRooms;
   rooms: any = [];
 
-  hotels_names = ["response-dezameron.json", "response-udeainn.json", "response-colombiaresort.json"]
-
-
-  constructor(public navCtrl: NavController, private _hotelzProvider: HotelzProvider) {
+  constructor(public navCtrl: NavController) {
     this.request = {} as RequestSearchRooms;
   }
 
-  getRooms(event) {
-    let result: any[] = []
-    new Promise((resolve, reject)=>{
-      for (let hotel_name of this.hotels_names) {
-        this._hotelzProvider.getAvalaibleRooms(hotel_name).then((response) => {
-          result.push(response)
-        })
-      }
-      this.navCtrl.push(ListRoomsPage, {response: result})
-    });
+  getRooms(evento) {
+    this.navCtrl.push(ListRoomsPage);
   }
+
 
 }
--- src/pages/list-rooms/list-rooms.ts
+++ src/pages/list-rooms/list-rooms.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HotelzProvider } from "../../providers/hotelz/hotelz";
+import { HotelzProvider } from '../../providers/hotelz/hotelz';
 
 @IonicPage()
 @Component({
@@ -9,13 +9,53 @@ import { HotelzProvider } from "../../providers/hotelz/hotelz";
 })
 export class ListRoomsPage {
 
-  response: any
+  hotels_response: any = []
   onlyRooms = []
+  hotels_names = ["response-dezameron.json", "response-udeainn.json", "response-colombiaresort.json"]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.response = navParams.get('response');
-    this.onlyRooms = this.response.rooms
-    console.log(this.onlyRooms)
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private _hotelzProvider: HotelzProvider) {
+    this.getRooms()
   }
 
+  getRooms() {
+    new Promise((resolve, reject)=>{
+      for (let hotel_name of this.hotels_names) {
+        this._hotelzProvider.getAvalaibleRooms(hotel_name).then((response) => {
+          this.hotels_response.push(response)
+          this.createListRooms(response)
+        })
+      }
+    });
+  }
+
+  createListRooms(hotel_response:any){
+      let hotel = hotel_response
+      console.log("Hola mundo")
+      for(let room of hotel.rooms){
+        let newRoom = {
+          hotel_name: hotel.hotel_name,
+          hotel_location: hotel.hotel_location.address,
+          room_type: room.room_type,
+          price: room.price,
+          room_thumbnail: room.room_thumbnail,
+          currency: room.currency
+        }
+        this.onlyRooms.push(newRoom)
+      }
+      this.ordenar()
+  }
+
+
+  ordenar(){
+    this.onlyRooms.sort((a, b) => {
+      if (a.price > b.price) {
+        return 1;
+      }
+      if (a.price < b.price) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 }
