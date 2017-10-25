4800ab5122be12e084869656c5c9c5dff862cf55
yoinergomez <yoiner.gomez22@gmail.com>
2017-10-24 22:20:18 -0500

    Add id to rooms response

--- src/pages/list-rooms/list-rooms.ts
+++ src/pages/list-rooms/list-rooms.ts
@@ -13,7 +13,8 @@ export class ListRoomsPage {
   info: any;
   hotels_response: any = []
   onlyRooms = []
-  hotels_names = ["https://udeain.herokuapp.com", "response-udeainn.json", "response-colombiaresort.json"]
+  //hotels_names = ["https://udeain.herokuapp.com/api/v1/rooms", "https://hotelz-python-api.herokuapp.com/V1/rooms/"]
+  hotels_names = ["https://udeain.herokuapp.com/api/v1/rooms"]
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private _hotelzProvider: HotelzProvider) {
@@ -25,6 +26,8 @@ export class ListRoomsPage {
     new Promise((resolve, reject)=>{
       for (let hotel_name of this.hotels_names) {
         this._hotelzProvider.getAvalaibleRooms(hotel_name, this.info).then((response) => {
+          console.log(response.id);
+
           this.hotels_response.push(response)
           this.createListRooms(response)
         })
