0db934df05cfd7dd96ab771284e9bb3062f12628
Maria Camila Gómez <camigomez@experimentality.co>
2017-11-11 00:22:49 -0500

    Add disign of my profile page

--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the MyProfilePage page.
@@ -15,8 +16,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formReserve: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.formReserve = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(7) ,Validators.maxLength(50), Validators.pattern('[a-zA-Z ]+')]],
+      doc_type: [null, [Validators.required]],
+      doc_id: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(11)]],
+      email: ['', [Validators.required, Validators.email, Validators.minLength(7), Validators.maxLength(50)]],
+      phone_number: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(7), Validators.maxLength(15)]]
+  });
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyProfilePage');
