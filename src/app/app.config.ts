341c8aa9fd449b7fddb45a3b53f9d3158c968992
yoinergomez <yoiner.gomez22@gmail.com>
2017-11-21 22:04:40 -0500

    Add Firebase credentials

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+export class AppSettings {
+  public static FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyDMhtNdY8NKO5xJ1vIfYPgBUFC7kYbGwmE',
+    authDomain: 'hotelz-develop.firebaseapp.com',
+    databaseURL: 'https://hotelz-develop.firebaseio.com',
+    projectId: 'hotelz-develop',
+    storageBucket: '',
+    messagingSenderId: '502482367855'
+  };
+}
