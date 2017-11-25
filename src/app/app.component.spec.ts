68b7f7e7f7760f844bce8f73d2d4698f7b592345
yoinergomez <yoiner.gomez22@gmail.com>
2017-11-25 00:44:35 -0500

    Resolve test for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 import { async, TestBed } from '@angular/core/testing';
 import { IonicModule, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -24,6 +25,7 @@ describe('MyApp Component', () => {
         { provide: StatusBar, useClass: StatusBarMock },
         { provide: SplashScreen, useClass: SplashScreenMock },
         { provide: Platform, useClass: PlatformMock }
+        { provide: AuthServiceProvider, useClass: PlatformMock }
       ]
     })
   }));
