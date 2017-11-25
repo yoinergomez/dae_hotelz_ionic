e1832ece37d2e6363f07efdef91a46dde9097e38
Maria Camila Gómez <camigomez@experimentality.co>
2017-11-24 22:17:11 -0500

    Create slide with Data user form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { ViewChild, AfterViewInit } from '@angular/core';
 
@@ -11,8 +12,16 @@ import { ViewChild, AfterViewInit } from '@angular/core';
 export class LoginPage implements AfterViewInit{
 
   @ViewChild(Slides) slides: Slides;
+  formReserve: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.formReserve = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(7) ,Validators.maxLength(50), Validators.pattern('[a-zA-Z ]+')]],
+        doc_type: [null, [Validators.required]],
+        doc_id: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(11)]],
+        email: ['', [Validators.required, Validators.email, Validators.minLength(7), Validators.maxLength(50)]],
+        phone_number: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(7), Validators.maxLength(15)]]
+    });
   }
 
   goToSlide() {
--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -20,7 +20,7 @@ export class MyProfilePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
     this.formReserve = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(7) ,Validators.maxLength(50), Validators.pattern('[a-zA-Z ]+')]],
+      name: ['', [Validators.required, Validators.minLength(7) ,Validators.maxLength(50), Validators.pattern('[a-zA-Z]+')]],
       doc_type: [null, [Validators.required]],
       doc_id: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(11)]],
       email: ['', [Validators.required, Validators.email, Validators.minLength(7), Validators.maxLength(50)]],
