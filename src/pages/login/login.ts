5786ec54fef9c08f406df0036a405192ead7d0a6
yoinergomez <yoiner.gomez22@gmail.com>
2017-11-25 00:37:05 -0500

    Create logic in login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,26 +1,54 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams, Slides } from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-import { ViewChild, AfterViewInit } from '@angular/core';
+import { ViewChild, AfterViewInit } from "@angular/core";
+import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
+import { TabsPage } from "../tabs/tabs";
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
-export class LoginPage implements AfterViewInit{
-
+export class LoginPage implements AfterViewInit {
   @ViewChild(Slides) slides: Slides;
   formReserve: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private formBuilder: FormBuilder,
+    private _authService: AuthServiceProvider
+  ) {
     this.formReserve = this.formBuilder.group({
-        name: ['', [Validators.required, Validators.minLength(7) ,Validators.maxLength(50), Validators.pattern('[a-zA-Z ]+')]],
-        doc_type: [null, [Validators.required]],
-        doc_id: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(11)]],
-        email: ['', [Validators.required, Validators.email, Validators.minLength(7), Validators.maxLength(50)]],
-        phone_number: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(7), Validators.maxLength(15)]]
+      name: [
+        {value:"", disabled:true},
+        [Validators.required, Validators.minLength(7), Validators.maxLength(50)]
+      ],
+      doc_type: [null, [Validators.required]],
+      doc_id: [
+        "",
+        [Validators.required, Validators.minLength(7), Validators.maxLength(11)]
+      ],
+      email: [
+        {value:"", disabled:true},
+        [
+          Validators.required,
+          Validators.email,
+          Validators.minLength(7),
+          Validators.maxLength(50)
+        ]
+      ],
+      phone_number: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("[0-9]*"),
+          Validators.minLength(7),
+          Validators.maxLength(15)
+        ]
+      ]
     });
   }
 
@@ -28,20 +56,37 @@ export class LoginPage implements AfterViewInit{
     this.slides.slideTo(2, 500);
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.slides.lockSwipes(true);
     this.slides.freeMode = false;
     this.slides.paginationType = "progress";
   }
 
-  login(){
-
+  login() {
+    this._authService.loginWithGoogle().then(status => {
+      console.log("status?");
+      console.log(status);
+      this._authService.getCurrentSession().then((session: any) => {
+        console.log("session");
+        console.log(session.email);
+        this.formReserve.patchValue({
+          name: session.displayName,
+          email: session.email
+        });
+        this.slides.lockSwipes(false);
+        this.slides.slideNext();
+        this.slides.lockSwipes(true);
+      });
+    });
   }
 
-  ingresar(){
-    //Ir al home
+  signIn() {
+    this._authService
+      .saveUserInformation(this.formReserve.value)
+      .then(confirm => {
+        if (confirm === true) {
+          this.navCtrl.setRoot(TabsPage);
+        }
+      });
   }
-
-
-
 }
